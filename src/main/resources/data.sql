05b87c430c666fa970a82d32f74dd0c55fd0765c
Eiji Toriki <ageForEngi@outlook.jp>
2023-10-28 11:42:44 +0900

    タスク一覧の表示（controller, modelの作成）

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into issues (summary, description) values ('Bug A', 'There is a crucial bug.');
+insert into issues (summary, description) values ('Function B', 'Customers want an add function.');
+insert into issues (summary, description) values ('Slow Screen C', 'Speed up!');

54f45b9f5e8cb6229df6023c7611f1c8eb9b3068
Eiji Toriki <ageForEngi@outlook.jp>
2023-10-29 16:01:06 +0900

    タスク一覧の表示（controller, modelの作成）

--- src/main/java/com/example/issuetrackingsystem/web/issue/IssueController.java
+++ src/main/java/com/example/issuetrackingsystem/web/issue/IssueController.java
@@ -5,20 +5,44 @@ import com.example.issuetrackingsystem.domain.issue.IssueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
     private final IssueService issueService;
 
     // GET /issues
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model){
         model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
+    }
+
+    // GET /issues/creationForm
+    @GetMapping("/creationForm")
+    public String showCreationForm(@ModelAttribute IssueForm form){
+        return "issues/creationForm";
+    }
+
+    // POST /issues
+    @PostMapping
+    public String create(@Validated IssueForm issueForm, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()){
+            return showCreationForm(issueForm);
+        }
+        issueService.create(issueForm.getSummary(), issueForm.getDescription());
+        return "redirect:/issues";
+    }
+
+    // GET localhost:8080/issues/1
+    @GetMapping("{issueId}")
+    public String showDetail(@PathVariable("issueId") long issueId, Model model){
+        model.addAttribute("issue", issueService.findById(issueId));
+        return "issues/detail";
     }
 }
